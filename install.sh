fa5ed43b4baa5a8cd336c8bb9ed9ea4e8ec67cc1
Sebastian Łuc <koemirai@gmail.com>
2020-02-26 00:40:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "====> Install venv ."
-sudo apt install python3-venv
+echo "====> Install package ...."
+sudo pip3 install torch torchvision \
+            git+https://github.com/rtqichen/torchdiffeq \
+            numpy  cycler==0.10.0 kiwisolver==1.1.0 \
+            matplotlib==3.1.3 Pillow==7.0.0 pkg-resources \
+            pyparsing==2.4.6 python-dateutil==2.8.1 six==1.14.0
+            
+sudo apt install python3-tk  
+ 
+echo "====> Done ....."
 
-echo "====> Create Environment .."
-sudo python3 -m venv benchmark_script
-
-echo "====> Activate Environment ..."
-sudo source benchmark_script/bin/activate
-sudo bash pkg.sh
